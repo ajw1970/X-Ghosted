9bbe4d85be2c5851631d70b177f3e8177002bfa7
John Welty <john@weltyautomation.com>
2025-02-18 17:38:07 -0600

    Simplify test

--- src/getHangingChads.test.js
+++ src/getHangingChads.test.js
@@ -2,21 +2,13 @@
 //Find parent article container of each
 //Return if vertical line is present: div class .r-1bnu78o
 
-describe('Hanging Chad Selector Test', () => {
-    // Load HTML before each test
-    beforeEach(() => {
-        loadHTML('../samples/Home-Timeline-With-Replies-With-Suspect-Reply-To-DOGE.html');
-    });
+test('We can find reply to @DOGE', () => {
+    loadHTML('../samples/Home-Timeline-With-Replies-With-Suspect-Reply-To-DOGE.html');
 
-    // Clear document after each test
-    afterEach(() => {
-        document.documentElement.innerHTML = '';
-    });
+    const replyingDivs = Array.from(document.querySelectorAll('div'))
+        .filter(div => div.textContent.startsWith('Replying to '));
+    expect(replyingDivs).not.toBeNull();
+    expect(replyingDivs.length).toBe(1);
 
-    test('should find element by class name', () => {
-        const replyingDivs = Array.from(document.querySelectorAll('div'))
-            .filter(div => div.textContent.startsWith('Replying to '));
-        expect(replyingDivs).not.toBeNull();
-        expect(replyingDivs.length).toBe(1);
-    });
+    document.documentElement.innerHTML = '';
 });
