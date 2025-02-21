c08204cddeee10cec61fa3cafa75d5f960001d7b
John Welty <john@weltyautomation.com>
2025-02-21 05:54:24 -0600

    Add example and test for a reply to now deleted post

--- src/getHangingChads.test.js
+++ src/getHangingChads.test.js
@@ -146,3 +146,12 @@ test('We identify the unavailable post in this conversation thread', () => {
 
     document.documentElement.innerHTML = '';
 });
+
+test('We identify the deleted post in this conversation thread', () => {
+    loadHTML('../samples/Replied-To-Now-Deleted-Post.html');
+
+    const matchingArticles = findMatchingArticles(document);
+    expect(matchingArticles.length).toBe(1);
+
+    document.documentElement.innerHTML = '';
+});
