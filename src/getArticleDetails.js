1d1624016776dad01fa929786276c7f059068ccd
John Welty <john@weltyautomation.com>
2025-03-01 09:42:17 -0600

    Minor formatting

--- src/getArticleDetails.js
+++ src/getArticleDetails.js
@@ -6,9 +6,11 @@ function getArticleDetails(articleElement) {
 
     const href = articleElement.querySelector('.css-146c3p1.r-1loqt21 time').parentElement.getAttribute('href');
     const datetime = articleElement.querySelector('time').getAttribute('datetime');
+
     return {
         href: href,
         datetime: datetime
     }
 }
+
 module.exports = getArticleDetails;
