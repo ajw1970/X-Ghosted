24ecd61efc4983488c3c2b9c277ff708b4c52460
John Welty <john@weltyautomation.com>
2025-03-01 18:16:16 -0600

    Enhance getArticleDetails with tests

--- src/utils/getArticleDetails.js
+++ src/utils/getArticleDetails.js
@@ -4,11 +4,23 @@ function getArticleDetails(articleElement) {
         return;
     }
 
-    const href = articleElement.querySelector('.css-146c3p1.r-1loqt21 time').parentElement.getAttribute('href');
-    const datetime = articleElement.querySelector('time').getAttribute('datetime');
+    // Get User-Name div element
+    const userDiv = articleElement.querySelector('div[data-testid="User-Name"]');
+
+    const postLink = 'https://x.com' + userDiv.querySelector('.css-146c3p1.r-1loqt21 time').parentElement.getAttribute('href');
+    const datetime = userDiv.querySelector('time').getAttribute('datetime');
+    const spans = userDiv.querySelectorAll('span.css-1jxf684.r-bcqeeo.r-1ttztb7.r-qvutc0.r-poiln3');
+    //const spanTexts = Array.from(spans).map(span => span.textContent); // This was used to visualize the content of the spans we find
+    const displayName = spans.item(0).textContent; // or item(1) works too
+    // Strip leading @ from username if it exists
+    let userName = spans.item(3).textContent;
+    let userNameMatch = userName.match(/^@?(.+)/);
+    userName = userNameMatch ? userNameMatch[1] : userName;
 
     return {
-        href: href,
+        displayName: displayName,
+        userName: userName,
+        postLink: postLink,
         datetime: datetime
     }
 }
--- src/utils/getArticleDetails.test.js
+++ src/utils/getArticleDetails.test.js
@@ -8,22 +8,30 @@ test('We can get posts from sample data', () => {
     expect(articles.length).toBe(4);
 
     expect(getArticleDetails(articles.item(0))).toEqual({
-        href: "/ProphetJoshuaD/status/1895793341721186371",
+        displayName: "Prophet Joshua DeRuiter",
+        userName: "ProphetJoshuaD",
+        postLink: "https://x.com/ProphetJoshuaD/status/1895793341721186371",
         datetime: "2025-03-01T11:08:49.000Z"
     });
 
     expect(getArticleDetails(articles.item(1))).toEqual({
-        href: "/ApostleLucille/status/1895520569833009525",
+        displayName: "Lucille",
+        userName: "ApostleLucille",
+        postLink: "https://x.com/ApostleLucille/status/1895520569833009525",
         datetime: "2025-02-28T17:04:55.000Z"
     });
 
     expect(getArticleDetails(articles.item(2))).toEqual({
-        href: "/ApostleEric/status/1895511099434283310",
+        displayName: "Eric vonAnderseck",
+        userName: "ApostleEric",
+        postLink: "https://x.com/ApostleEric/status/1895511099434283310",
         datetime: "2025-02-28T16:27:17.000Z"
     });
 
     expect(getArticleDetails(articles.item(3))).toEqual({
-        href: "/catievanderwalt/status/1895383819655749937",
+        displayName: "Apostle Catie",
+        userName: "catievanderwalt",
+        postLink: "https://x.com/catievanderwalt/status/1895383819655749937",
         datetime: "2025-02-28T08:01:31.000Z"
     });
 
@@ -38,22 +46,30 @@ test('We can get replies from sample data', () => {
     expect(articles.length).toBe(4);
 
     expect(getArticleDetails(articles.item(0))).toEqual({
-        href: "/ProphetJoshuaD/status/1895796406956544359",
+        displayName: "Prophet Joshua DeRuiter",
+        userName: "ProphetJoshuaD",
+        postLink: "https://x.com/ProphetJoshuaD/status/1895796406956544359",
         datetime: "2025-03-01T11:21:00.000Z"
     });
 
     expect(getArticleDetails(articles.item(1))).toEqual({
-        href: "/NeethlingBert/status/1895783011716641099",
+        displayName: "Bert Neethling",
+        userName: "NeethlingBert",
+        postLink: "https://x.com/NeethlingBert/status/1895783011716641099",
         datetime: "2025-03-01T10:27:46.000Z"
     });
 
     expect(getArticleDetails(articles.item(2))).toEqual({
-        href: "/NeethlingBert/status/1895766475425923528",
+        displayName: "Bert Neethling",
+        userName: "NeethlingBert",
+        postLink: "https://x.com/NeethlingBert/status/1895766475425923528",
         datetime: "2025-03-01T09:22:04.000Z"
     });
 
     expect(getArticleDetails(articles.item(3))).toEqual({
-        href: "/TeacherColleen/status/1895615009159594130",
+        displayName: "Colleen Bucknell",
+        userName: "TeacherColleen",
+        postLink: "https://x.com/TeacherColleen/status/1895615009159594130",
         datetime: "2025-02-28T23:20:11.000Z"
     });
 
