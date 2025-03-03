9c323d7e035808889f6e5fb633f53f790987087e
John Welty <john@weltyautomation.com>
2025-03-03 16:21:54 -0600

    Terminal Add tested ability to extract datetime

--- src/getArticleDetails.js
+++ src/getArticleDetails.js
@@ -5,8 +5,10 @@ function getArticleDetails(articleElement) {
     }
 
     const href = articleElement.querySelector('.css-146c3p1.r-1loqt21 time').parentElement.getAttribute('href');
+    const datetime = articleElement.querySelector('time').getAttribute('datetime');
     return {
-        href: href
+        href: href,
+        datetime: datetime
     }
 }
 module.exports = getArticleDetails;
