1c4ded2c4c53db90cd16392fa6d194f45d22693e
John Welty <john@weltyautomation.com>
2025-03-08 14:16:11 -0600

    This gets changes noticed and updates mostly working

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,7 +4,7 @@ import replace from 'gulp-replace';
 import prettier from 'gulp-prettier';
 import fs from 'fs';
 import bump from 'gulp-bump';
-import { resolve } from 'path';  // Add this import
+import { resolve } from 'path';
 
 // Function to read and clean utility file content
 function readAndCleanFile(filePath) {
@@ -18,20 +18,31 @@ const articleLinksToTargetCommunities = readAndCleanFile('src/utils/articleLinks
 const findReplyingToWithDepth = readAndCleanFile('src/utils/findReplyingToWithDepth.js');
 
 // Task to bump version in package.json and sync with template
-gulp.task('bump-version', function() {
-    return gulp.src('./package.json')
-        .pipe(bump({ type: 'patch' })) // You can change to 'minor' or 'major' as needed
+gulp.task('bump-version', function(done) {
+    gulp.src('./package.json')
+        .pipe(bump({ type: 'patch' }))
         .pipe(gulp.dest('./'))
-        .pipe(gulp.src('src/highlight-potential-problems-template.js')
-            .pipe(replace(
-                /version:\s*['"]\d+\.\d+\.\d+['"]/,
-                () => {
-                    const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-                    return `version: '${packageJson.version}'`;
-                }
-            ))
-            .pipe(gulp.dest('src'))
-        );
+        .on('end', () => {
+            // After package.json is updated, update the template
+            gulp.src('src/highlight-potential-problems-template.js')
+                .pipe(replace(
+                    /version:\s*['"]\d+\.\d+\.\d+['"]/,
+                    () => {
+                        const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+                        return `version: '${packageJson.version}'`;
+                    }
+                ))
+                .pipe(gulp.dest('src'))
+                .on('error', (err) => {
+                    console.error('Error in bump-version:', err);
+                    done(err);
+                })
+                .on('end', done); // Signal completion when the nested stream finishes
+        })
+        .on('error', (err) => {
+            console.error('Error in bump-version:', err);
+            done(err);
+        });
 });
 
 // Task to build the output file
@@ -45,7 +56,7 @@ gulp.task('build', function() {
         .pipe(gulp.dest('src'));
 });
 
-// Watch task to monitor changes in specific files and trigger version bump and build
+// Watch task with debugging
 gulp.task('watch', function() {
     const files = [
         'src/highlight-potential-problems-template.js',
@@ -53,10 +64,21 @@ gulp.task('watch', function() {
         'src/utils/articleLinksToTargetCommunities.js',
         'src/utils/findReplyingToWithDepth.js'
     ];
-    // Use ES Module path resolution instead of require
+    console.log('Starting watch task...');
     console.log('Watching files:', files.map(f => resolve(f)));
-    return gulp.watch(files, gulp.series('bump-version', 'build'));
+    const watcher = gulp.watch(files, { usePolling: true, interval: 1000 }, gulp.series('bump-version', 'build'));
+    watcher.on('change', function(path, stats) {
+        console.log(`File ${path} was changed, running tasks...`);
+    });
+    watcher.on('add', function(path) {
+        console.log(`File ${path} was added`);
+    });
+    watcher.on('unlink', function(path) {
+        console.log(`File ${path} was deleted`);
+    });
+    console.log('Watcher initialized');
+    return watcher;
 });
 
-// Default task to run the watch task
+// Default task
 gulp.task('default', gulp.series('watch'));
