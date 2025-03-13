18a45bc35750b344a59bba919daad021420926d6
John Welty <john@weltyautomation.com>
2025-03-13 02:38:57 -0500

    Update jest setup provide TextEncoder

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const { TextEncoder, TextDecoder } = require('util');
 
 // Function to load HTML file into JSDOM
 global.loadHTML = (filePath) => {
   const html = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
   document.documentElement.innerHTML = html;
 };
+
+// Add TextEncoder and TextDecoder to the global scope
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
