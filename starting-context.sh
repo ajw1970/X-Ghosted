2f1f6639d7c6f6c3b8df7904949357194ee4a26e
John Welty <john@weltyautomation.com>
2025-03-23 11:35:47 -0500

    Add script to build grok starting context

---
+++ starting-context.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# filepath: c:\Dev\X-Twitter\Ghosted\starting-context.sh
+
+# Run the Node.js script with the specified arguments
+node grokify.js grok/_*.adoc src/xGhosted.test.js starting-context.txt
