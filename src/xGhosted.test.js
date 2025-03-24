173e92c1c288c76d4d10f28727e0aa9d6d9238a3
John Welty <john@weltyautomation.com>
2025-03-24 09:56:38 -0500

    Revert this goof from 7a7d75

--- src/xGhosted.test.js
+++ src/xGhosted.test.js
@@ -75,7 +75,143 @@ describe('xGhosted', () => {
     expect(analyses[0].quality).toEqual(postQuality.GOOD);
     expect(analyses[0].reason).toEqual("Looks good");
     expect(analyses[0].link).toEqual("/DongWookChung2/status/1887852588457988314");
-    // ... rest of assertions unchanged ...
+
+    expect(analyses[1].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[1].reason).toEqual("Looks good"); 
+    expect(analyses[1].link).toEqual("/monetization_x/status/1897010659075989835"); 
+ 
+    expect(analyses[2].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[2].reason).toEqual("Looks good"); 
+    expect(analyses[2].link).toEqual("/ApostleJohnW/status/1897016048639180873"); 
+ 
+    expect(analyses[3].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[3].reason).toEqual("No article found"); 
+    expect(analyses[3].link).toEqual("/ApostleJohnW/status/1897016048639180873#filler1"); 
+ 
+    expect(analyses[4].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[4].reason).toEqual("Looks good"); 
+    expect(analyses[4].link).toEqual("/Name__Error_404/status/1896938936599228642"); 
+ 
+    expect(analyses[5].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[5].reason).toEqual("No article found"); 
+    expect(analyses[5].link).toEqual("/Name__Error_404/status/1896938936599228642#filler1"); 
+ 
+    expect(analyses[6].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[6].reason).toEqual("Looks good"); 
+    expect(analyses[6].link).toEqual("/Name__Error_404/status/1897015679158788554"); 
+ 
+    expect(analyses[7].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[7].reason).toEqual("Looks good"); 
+    expect(analyses[7].link).toEqual("/ApostleJohnW/status/1897015899099414914"); 
+ 
+    expect(analyses[8].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[8].reason).toEqual("No article found"); 
+    expect(analyses[8].link).toEqual("/ApostleJohnW/status/1897015899099414914#filler1"); 
+ 
+    expect(analyses[9].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[9].reason).toEqual("Looks good"); 
+    expect(analyses[9].link).toEqual("/Name__Error_404/status/1897015203541524847"); 
+ 
+    expect(analyses[10].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[10].reason).toEqual("Looks good"); 
+    expect(analyses[10].link).toEqual("/ApostleJohnW/status/1897015449176748449"); 
+ 
+    expect(analyses[11].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[11].reason).toEqual("No article found"); 
+    expect(analyses[11].link).toEqual("/ApostleJohnW/status/1897015449176748449#filler1"); 
+ 
+    expect(analyses[12].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[12].reason).toEqual("Looks good"); 
+    expect(analyses[12].link).toEqual("/SpaceX/status/1896708396902174849"); 
+ 
+    expect(analyses[13].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[13].reason).toEqual("Looks good"); 
+    expect(analyses[13].link).toEqual("/ApostleJohnW/status/1897003945203306614"); 
+ 
+    expect(analyses[14].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[14].reason).toEqual("Looks good"); 
+    expect(analyses[14].link).toEqual("/ApostleJohnW/status/1897013413664145793"); 
+ 
+    expect(analyses[15].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[15].reason).toEqual("No article found"); 
+    expect(analyses[15].link).toEqual("/ApostleJohnW/status/1897013413664145793#filler1"); 
+ 
+    expect(analyses[16].quality).toEqual(postQuality.PROBLEM); 
+    expect(analyses[16].reason).toEqual("Found notice: this post is unavailable"); 
+    expect(analyses[16].link).toEqual("/OwenGregorian/status/1896977661144260900"); 
+ 
+    expect(analyses[17].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[17].reason).toEqual("Looks good"); 
+    expect(analyses[17].link).toEqual("/ApostleJohnW/status/1897011110072738182"); 
+ 
+    expect(analyses[18].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[18].reason).toEqual("No article found"); 
+    expect(analyses[18].link).toEqual("/ApostleJohnW/status/1897011110072738182#filler1"); 
+ 
+    expect(analyses[19].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[19].reason).toEqual("Looks good"); 
+    expect(analyses[19].link).toEqual("/DongWookChung2/status/1897005083709374868"); 
+ 
+    expect(analyses[20].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[20].reason).toEqual("Looks good"); 
+    expect(analyses[20].link).toEqual("/ApostleJohnW/status/1897010202974806174"); 
+ 
+    expect(analyses[21].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[21].reason).toEqual("No article found"); 
+    expect(analyses[21].link).toEqual("/ApostleJohnW/status/1897010202974806174#filler1"); 
+ 
+    expect(analyses[22].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[22].reason).toEqual("Looks good"); 
+    expect(analyses[22].link).toEqual("/monetization_x/status/1896999071665324318"); 
+ 
+    expect(analyses[23].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[23].reason).toEqual("No article found"); 
+    expect(analyses[23].link).toEqual("/monetization_x/status/1896999071665324318#filler1"); 
+ 
+    expect(analyses[24].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[24].reason).toEqual("Looks good"); 
+    expect(analyses[24].link).toEqual("/godswayfoundinc/status/1897003429870129243"); 
+ 
+    expect(analyses[25].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[25].reason).toEqual("Looks good"); 
+    expect(analyses[25].link).toEqual("/ApostleJohnW/status/1897004848614420667"); 
+ 
+    expect(analyses[26].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[26].reason).toEqual("No article found"); 
+    expect(analyses[26].link).toEqual("/ApostleJohnW/status/1897004848614420667#filler1"); 
+ 
+    expect(analyses[27].quality).toEqual(postQuality.POTENTIAL_PROBLEM); 
+    expect(analyses[27].reason).toEqual("Found: 'Replying to <a>@godswayfoundinc</a> and <a>@monetization_x</a>' at a depth of 6"); 
+    expect(analyses[27].link).toEqual("/ApostleJohnW/status/1897004713570394503"); 
+ 
+    expect(analyses[28].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[28].reason).toEqual("No article found"); 
+    expect(analyses[28].link).toEqual("/ApostleJohnW/status/1897004713570394503#filler1"); 
+ 
+    expect(analyses[29].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[29].reason).toEqual("Looks good"); 
+    expect(analyses[29].link).toEqual("/godswayfoundinc/status/1897002671846121539"); 
+ 
+    expect(analyses[30].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[30].reason).toEqual("Looks good"); 
+    expect(analyses[30].link).toEqual("/ApostleJohnW/status/1897002963107025141"); 
+ 
+    expect(analyses[31].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[31].reason).toEqual("No article found"); 
+    expect(analyses[31].link).toEqual("/ApostleJohnW/status/1897002963107025141#filler1"); 
+ 
+    expect(analyses[32].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[32].reason).toEqual("Looks good"); 
+    expect(analyses[32].link).toEqual("/WesleyKy/status/1896999314582642895"); 
+ 
+    expect(analyses[33].quality).toEqual(postQuality.GOOD); 
+    expect(analyses[33].reason).toEqual("Looks good"); 
+    expect(analyses[33].link).toEqual("/ApostleJohnW/status/1897002818214748430"); 
+ 
+    expect(analyses[34].quality).toEqual(postQuality.UNDEFINED); 
+    expect(analyses[34].reason).toEqual("No article found"); 
+    expect(analyses[34].link).toEqual("/ApostleJohnW/status/1897002818214748430#filler1"); 
+ 
     expect(analyses[35].quality).toEqual(postQuality.POTENTIAL_PROBLEM);
     expect(analyses[35].reason).toEqual("Found: 'Replying to <a>@monetization_x</a>' at a depth of 6");
     expect(analyses[35].link).toEqual("/ApostleJohnW/status/1897002239753073002");
