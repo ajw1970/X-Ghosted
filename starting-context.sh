61f16e3ad2678f237cfd59aab5b74fcd04394569
John Welty <john@weltyautomation.com>
2025-03-24 14:17:04 -0500

    Update starting-context.sh to streamline script execution and add line count output

--- starting-context.sh
+++ starting-context.sh
@@ -2,4 +2,6 @@
 # filepath: c:\Dev\X-Twitter\Ghosted\starting-context.sh
 
 # Run the Node.js script with the specified arguments
-node grokify.js grok/_*.adoc src/xGhosted.test.js starting-context.txt
+node undoc.js
+node grokify.js src/xGhosted.test.js starting-code.txt
+wc -l grok/*.adoc.txt grok/starting-code.txt > grok/line-counts.txt
