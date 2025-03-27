c148cedcc8ab44ddd5a493fd3accc7be9b179257
John Welty <john@weltyautomation.com>
2025-03-27 10:11:56 -0500

    Remove commented logger statements in identifyPost function

--- src/utils/identifyPost.js
+++ src/utils/identifyPost.js
@@ -37,22 +37,22 @@ function identifyPost(post, checkReplies = false, logger = console.log) {
     if (checkReplies) {
         // Posts with "Replying to" might be potential problems when found on with_replies page
         const replyingToDepths = findReplyingToWithDepth(article);
-        logger(`Checking replies for post, found ${replyingToDepths.length} "Replying to" instances:`, replyingToDepths);
+        // logger(`Checking replies for post, found ${replyingToDepths.length} "Replying to" instances:`, replyingToDepths);
         if (Array.isArray(replyingToDepths) && replyingToDepths.length > 0) {
             // Posts with replying to found at a depth < 10 are potential problems
             const replyingTo = replyingToDepths.find(object => object.depth < 10);
             if (replyingTo) {
-                logger(`POTENTIAL_PROBLEM detected: '${replyingTo.innerHTML}' at depth ${replyingTo.depth}`);
+                // logger(`POTENTIAL_PROBLEM detected: '${replyingTo.innerHTML}' at depth ${replyingTo.depth}`);
                 return {
                     quality: postQuality.POTENTIAL_PROBLEM,
                     reason: `Found: '${replyingTo.innerHTML}' at a depth of ${replyingTo.depth}`,
                     link: getRelativeLinkToPost(post),
                 };
             } else {
-                logger('No "Replying to" found at depth < 10');
+                // logger('No "Replying to" found at depth < 10');
             }
         } else {
-            logger('No "Replying to" divs found');
+            // logger('No "Replying to" divs found');
         }
     }
 
