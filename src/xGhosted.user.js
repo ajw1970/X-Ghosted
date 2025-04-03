c7ef9ada182b84937d6dc7e69417d8a07a2d59f6
John Welty <john@weltyautomation.com>
2025-04-03 11:40:54 -0500

    generate user script

--- src/xGhosted.user.js
+++ src/xGhosted.user.js
@@ -173,9 +173,7 @@
 
   // src/utils/getRelativeLinkToPost.js
   function getRelativeLinkToPost(element) {
-    const link = element
-      .querySelector('.css-146c3p1.r-1loqt21 time')
-      ?.parentElement?.getAttribute('href');
+    const link = element.querySelector('a:has(time)').getAttribute('href');
     return link || false;
   }
 
