dc35b652d009f1502dd44e1b4a66a74b78747f54
John Welty <john@weltyautomation.com>
2025-05-03 11:49:01 -0500

    Update scrollInterval and scrollPercentage in configuration for improved performance

--- src/utils/PollingManager.test.js
+++ src/utils/PollingManager.test.js
@@ -4,6 +4,29 @@ import { CONFIG } from "../config.js";
 import { EVENTS } from "../events.js";
 import { domUtils } from "../dom/domUtils.js";
 
+vi.mock("../config.js", () => ({
+  CONFIG: {
+    timing: {
+      debounceDelay: 500,
+      throttleDelay: 1000,
+      tabCheckThrottle: 5000,
+      exportThrottle: 5000,
+      rateLimitPause: 20 * 1000,
+      pollInterval: 500,
+      scrollInterval: 800,
+      isPostScanningEnabledOnStartup: false, // We'll send an event to change to true on the first heartbeat poll
+      userRequestedAutoScrollOnStartup: false,
+    },
+    showSplash: true,
+    logTarget: "tampermonkey",
+    persistProcessedPosts: false,
+    linkPrefix: "https://x.com",
+    debug: false,
+    smoothScrolling: true,
+    scrollPercentage: 0.9,
+  },
+}));
+
 describe("PollingManager", () => {
   let pollingManager;
   let mockDocument;
